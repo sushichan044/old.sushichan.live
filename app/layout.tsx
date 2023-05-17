5ba46b72be7ce453b542396fd2ac91ed5d0afafb
sushi-chaaaan <mail@sushichan.live>
2023-05-18 08:45:39 +0900

    Migrate into stylelint15

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
-import './globals.scss'
+import '@/styles/globals.scss'
+
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main>{children}</main>
+      </body>
     </html>
   )
 }
