1ef12752e2bd29f2bdaeae118f5b778e6e983858
sushi-chaaaan <mail@sushichan.live>
2023-05-18 08:45:54 +0900

    Format with prettier

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-const path = require("path");
+const path = require('path')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
