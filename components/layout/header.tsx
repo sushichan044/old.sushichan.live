ab255efa54e957783d1a19d3072a60d0f90fa758
sushi-chaaaan <mail@sushichan.live>
2023-05-18 22:28:14 +0900

    add NavigationMenu

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,9 +1,11 @@
 import styles from '@/components/layout/header.module.scss'
+import NavigationMenu from '@/components/navigation/navigationMenu'
 
 const Header = () => {
   return (
     <header className={styles.container}>
       <p>Header</p>
+      <NavigationMenu />
     </header>
   )
 }
---
+++ components/navigation/navigationMenu.tsx
@@ -0,0 +1,42 @@
+'use client'
+import * as NavMenu from '@radix-ui/react-navigation-menu'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { ComponentProps } from 'react'
+
+import styles from '@/components/navigation/styles.module.scss'
+
+type NavLinkProps = Omit<ComponentProps<typeof NavMenu.Link>, 'href'> & {
+  href: ComponentProps<typeof Link>['href']
+}
+
+const NavLink: React.FC<NavLinkProps> = ({ href, children, ...props }) => {
+  const isActive = usePathname() === href
+  return (
+    <Link href={href} passHref>
+      <NavMenu.Link active={isActive} asChild {...props} /> {children}
+    </Link>
+  )
+}
+
+const NavigationMenu = () => {
+  return (
+    <NavMenu.Root className={styles['menu-root']}>
+      <NavMenu.List className={styles['menu-list']}>
+        <NavMenu.Item>
+          <NavLink href="/">
+            <p>home</p>
+          </NavLink>
+        </NavMenu.Item>
+        <NavMenu.Item>
+          <NavLink href="/blog">
+            <p>blog</p>
+          </NavLink>
+        </NavMenu.Item>
+        <NavMenu.Indicator />
+      </NavMenu.List>
+    </NavMenu.Root>
+  )
+}
+
+export default NavigationMenu
