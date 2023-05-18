340fd746d09cc3e6629a227a48a4b4253632d710
sushi-chaaaan <mail@sushichan.live>
2023-05-19 00:07:11 +0900

    reduce font

--- lib/font.ts
+++ lib/font.ts
@@ -1,33 +1,26 @@
-import { Fira_Code, Lato, Noto_Sans_JP, Roboto } from 'next/font/google'
-
-const lato = Lato({
-  display: 'swap',
-  weight: ['100', '300', '400', '700', '900'],
-  subsets: ['latin'],
-  variable: '--font-lato',
-})
+import { Fira_Code, Noto_Sans_JP, Roboto } from 'next/font/google'
 
 const notoSansJP = Noto_Sans_JP({
   display: 'swap',
-  weight: ['100', '300', '400', '700', '900'],
+  weight: ['100', '400', '700', '900'],
   subsets: ['latin'],
   variable: '--font-noto-sans-jp',
 })
 
 const roboto = Roboto({
   display: 'swap',
-  weight: ['100', '300', '400', '700', '900'],
+  weight: ['100', '400', '700', '900'],
   subsets: ['latin'],
   variable: '--font-roboto',
 })
 
 const firaCode = Fira_Code({
   display: 'swap',
-  weight: ['300', '400', '500', '600', '700'],
+  weight: ['400', '700'],
   subsets: ['latin'],
   variable: '--font-fira-code',
 })
 
-export const fontVariables = [lato, notoSansJP, roboto, firaCode]
+export const fontVariables = [notoSansJP, roboto, firaCode]
   .map((font) => font.variable)
   .join(' ')
