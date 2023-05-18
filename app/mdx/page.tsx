ed117db4fd6158650227ec869b1e4ccbe761015e
sushi-chaaaan <mail@sushichan.live>
2023-05-18 18:22:25 +0900

    Try page with mdx

---
+++ app/mdx/page.tsx
@@ -0,0 +1,15 @@
+import MDX from '@/app/mdx/post.mdx'
+
+const Page = () => {
+  return (
+    <div>
+      <MDX />
+    </div>
+  )
+}
+
+export const metadata = {
+  title: 'MDX Page',
+}
+
+export default Page
