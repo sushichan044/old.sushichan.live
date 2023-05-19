840a82e31964d3a3f5621b5335e1ffd758e40c49
sushi-chaaaan <mail@sushichan.live>
2023-05-19 11:20:55 +0900

    Add fileHasExtension

--- lib/fs.ts
+++ lib/fs.ts
@@ -1,4 +1,31 @@
 import fs from 'fs'
+/**
+ * check if file has one of the expected extensions.
+ * @param {string} file file name. e.g. `index.mdx`.
+ * @param {string []} expectedExtensions
+ * * list of expected extensions. e.g. `['.mdx', '.md']`.
+ * * If the extension do not started with `'.'`, it will be automatically added.
+ * * e.g. `['mdx', 'md']` will be converted to `['.mdx', '.md']`.
+ * @returns {string | null} file name with extension or null if not found.
+ * @example
+ * fileHasExtension('index.mdx', ['.mdx', '.md']) // returns 'index.mdx'
+ * fileHasExtension('index.zip', ['.mdx', '.md']) // returns null
+ */
+export const fileHasExtension = (
+  file: string,
+  expectedExtensions: string[]
+): string | null => {
+  for (const ext of expectedExtensions) {
+    if (!file.startsWith('.')) {
+      file = `.${file}`
+    }
+
+    if (file.endsWith(ext)) {
+      return file
+    }
+  }
+  return null
+}
 
 export const recursiveGetFilepath = async (dir: string): Promise<string[]> => {
   const recursiveFiles: string[] = []
