43bd12669a025887e5637a23c39414c6b03ce927
sushi-chaaaan <mail@sushichan.live>
2023-05-19 16:17:29 +0900

    Add microCMS as imageRemote

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const path = require('path')
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['images.microcms-assets.io'],
+  },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
