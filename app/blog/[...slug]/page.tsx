6d1b85864cb03c00064101f653efa7d6cad4bb99
sushi-chaaaan <mail@sushichan.live>
2023-05-19 11:22:06 +0900

    Use catch-all route and nested route for mdx

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -4,22 +4,22 @@ import { checkMDXExistence, compileMDX, getAllMDXSlugs } from '@/lib/mdx'
 
 type PageProps = {
   params: {
-    slug: string
+    slug: string[]
   }
 }
 
 export async function generateStaticParams() {
   const slugs = await getAllMDXSlugs()
-  return slugs?.map((slug) => ({ slug: slug }))
+  return slugs?.map((slug) => ({ slug: slug.split('/') }))
 }
 
 export default async function Page({ params: { slug } }: PageProps) {
-  const mdx = checkMDXExistence(slug)
+  const mdxPath = slug.join('/')
+  const mdx = checkMDXExistence(mdxPath)
   if (!mdx.exists) {
     notFound()
   }
-
-  const { content, frontmatter } = await compileMDX(slug, mdx.extension)
+  const { content, frontmatter } = await compileMDX(mdxPath, mdx.extension)
   return (
     <>
       {frontmatter && (
