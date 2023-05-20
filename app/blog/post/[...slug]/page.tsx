718214438de71442909421438c780f6126b2cdc7
sushi-chaaaan <mail@sushichan.live>
2023-05-21 05:20:00 +0900

    Fix fileName

--- app/blog/post/[...slug]/page.tsx
+++ app/blog/post/[...slug]/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import styles from '@/app/blog/post/[...slug]/page.module.scss'
 import FrontMatter from '@/components/mdx/frontMatter'
 import MDXImage from '@/components/mdx/image'
-import NotFoundMeta from '@/components/meta/404'
+import NotFoundMeta from '@/components/meta/notFound'
 import {
   checkMDXExistence,
   compileMDX,
