46078fbbb81cf5cb29679974f521df4a44d43d5a
sushi-chaaaan <mail@sushichan.live>
2023-05-21 02:48:28 +0900

    set link style in header

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import styles from '@/components/layout/header.module.scss'
 import NavigationMenu from '@/components/navigation/navigationMenu'
 
@@ -5,7 +7,9 @@ const Header = () => {
   return (
     <header className={styles.container}>
       <div className={styles['header-inside']}>
-        <p className={styles.title}>お寿司だいすきチャンネル</p>
+        <Link href="/">
+          <p className={styles.title}>お寿司だいすきチャンネル</p>
+        </Link>
         <NavigationMenu />
       </div>
     </header>
