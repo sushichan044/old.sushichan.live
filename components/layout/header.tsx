67cd888648eaf9d3b34db9db531fbae30ae52e27
sushi-chaaaan <mail@sushichan.live>
2023-05-21 05:01:28 +0900

    Set header icon

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,15 +1,19 @@
+import Image from 'next/image'
 import Link from 'next/link'
 
 import styles from '@/components/layout/header.module.scss'
 import NavigationMenu from '@/components/navigation/navigationMenu'
+import LogoSvg from '@/public/logo.svg'
 
 const Header = () => {
   return (
     <header className={styles.container}>
       <div className={styles['header-inside']}>
-        <Link href="/">
+        <Link className={styles.hero} href="/">
+          <Image alt="site logo" height={50} src={LogoSvg} width={50} />
           <p className={styles.title}>お寿司だいすきチャンネル</p>
         </Link>
+
         <NavigationMenu />
       </div>
     </header>
