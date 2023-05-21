7e181cc5516b9a8c5b2b9908bec6a10f76dc2872
sushi-chaaaan <mail@sushichan.live>
2023-05-21 15:31:38 +0900

    Add image remote pattern

--- next.config.js
+++ next.config.js
@@ -5,6 +5,18 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['images.microcms-assets.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.microcms-assets.io',
+        pathname: '/assets/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'imagedelivery.net',
+        pathname: '/i4QA6VLSP0gXyj6-3zNFXg/**',
+      },
+    ],
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
