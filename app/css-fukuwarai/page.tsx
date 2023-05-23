8b6417b8cd3d3a0bd9a55cbceb3a70369b0c4ea6
sushi-chaaaan <mail@sushichan.live>
2023-05-24 03:15:30 +0900

    Use server action to parse CSS

--- app/css-fukuwarai/page.tsx
+++ app/css-fukuwarai/page.tsx
@@ -1,22 +1,34 @@
 'use client'
 
-import { editor } from 'monaco-editor'
+import { css } from '@linaria/core'
 import dynamic from 'next/dynamic'
-import { useState } from 'react'
+import { useRef } from 'react'
+
+import { parseCSS } from '@/app/css-fukuwarai/action'
 
 export default async function Page() {
-  const [code, setCode] = useState('')
+  const codeRef = useRef<string>('')
+  const fieldRef = useRef<HTMLSpanElement>(null)
 
-  const Editor = dynamic(import('@monaco-editor/react'), { ssr: false })
+  const Editor = dynamic(async () => import('@monaco-editor/react'), {
+    ssr: false,
+  })
 
-  const handleChange = (
-    value: string | undefined,
-    event: editor.IModelContentChangedEvent
-  ) => {
-    if (!value || !event.isEolChange) return
-    console.log(value)
-    setCode(value)
+  const handleChange = (value: string | undefined) => {
+    if (!value) return
+    codeRef.current = value
   }
+
+  const handleCode = async (code: string) => {
+    if (!fieldRef.current) return
+    fieldRef.current.innerHTML = code
+    const AST = await parseCSS(code)
+    console.log(AST)
+  }
+
+  const mouth = css`
+    width: 100px;
+  `
 
   return (
     <>
@@ -27,8 +39,15 @@ export default async function Page() {
         width="50%"
       />
       <div>
-        <span>アヌアヌ</span>
-        {code}
+        <button onClick={() => handleCode(codeRef.current)}>アヌアヌ</button>
+        <span ref={fieldRef} />
+      </div>
+      <div className="face-root">
+        <div className="face">
+          <div className="right-eye">eye</div>
+          <div className="left-eye">eye</div>
+          <div className={mouth}>mouth</div>
+        </div>
       </div>
     </>
   )
