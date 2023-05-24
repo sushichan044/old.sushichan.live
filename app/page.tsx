2355728e6fb1786c12dbe3fb10ff165c5532cb7a
sushi-chaaaan <mail@sushichan.live>
2023-05-25 00:46:55 +0900

    Add link

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link'
+
 export default async function Home() {
   return (
     <div id="main-container">
       <p>Hello World!</p>
       <p>こんにちは世界！</p>
+      <Link href="/css-fukuwarai">
+        <p>CSS福笑い(α版,スマホ未対応)</p>
+      </Link>
     </div>
   )
 }
