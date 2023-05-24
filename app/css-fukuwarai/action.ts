9a63a19784866ef776de17fd437b37ff2b6381d0
sushi-chaaaan <mail@sushichan.live>
2023-05-24 18:38:42 +0900

    progress

--- app/css-fukuwarai/action.ts
+++ app/css-fukuwarai/action.ts
@@ -1,12 +1,67 @@
 'use server'
 
-import { parse } from 'css'
+import { parse, Rule } from 'css'
+
+const ALLOWED_SELECTORS = [
+  '.face-root',
+  '.face',
+  '.left-eye',
+  '.right-eye',
+  '.nose',
+  '.mouth',
+] as const
+
+export type ExtractedCSS = Omit<Rule, 'type' | 'selectors' | 'declarations'> & {
+  type: 'rule'
+  selectors: (typeof ALLOWED_SELECTORS)[number][]
+  declarations: {
+    type: 'declaration'
+    property: string
+    value: string
+    position: {
+      start: {
+        line: number
+        column: number
+      }
+      end: {
+        line: number
+        column: number
+      }
+    }
+  }[]
+}
 
 export const parseCSS = async (code: string): Promise<string> => {
   const cssAST = parse(code)
-  console.log(cssAST)
-  // TODO: parse AST Here
+  console.log('%j', cssAST)
 
-  const parsedCSS = 'アヌアヌ'
-  return parsedCSS
+  if (!cssAST.stylesheet) {
+    throw new Error('CSS AST is not valid')
+  }
+
+  if (
+    cssAST.stylesheet.parsingErrors &&
+    cssAST.stylesheet.parsingErrors.length > 0
+  ) {
+    throw new Error(
+      `CSS AST has some errors: ${JSON.stringify(
+        cssAST.stylesheet.parsingErrors
+      )}`
+    )
+  }
+  const extracted = cssAST.stylesheet.rules
+    .filter((rule): rule is Rule => rule.type === 'rule')
+    .filter((rule) => {
+      if (!rule.selectors) {
+        return false
+      }
+      return rule.selectors.every((selector) =>
+        ALLOWED_SELECTORS.some(
+          (allowedSelector) => selector === allowedSelector
+        )
+      )
+    }) as ExtractedCSS[]
+  console.log('%j', extracted)
+
+  return JSON.stringify(extracted)
 }
--- app/css-fukuwarai/page.tsx
+++ app/css-fukuwarai/page.tsx
@@ -1,12 +1,15 @@
 'use client'
 
 import { css } from '@linaria/core'
+// import { editor } from 'monaco-editor'
+// import { KeyCode, KeyMod } from 'monaco-editor/esm/vs/editor/editor.api'
 import dynamic from 'next/dynamic'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
-import { parseCSS } from '@/app/css-fukuwarai/action'
+// import { ExtractedCSS, parseCSS } from '@/app/css-fukuwarai/action'
 
 export default async function Page() {
+  const [fukuCSS, setFukuCSS] = useState<string>('')
   const codeRef = useRef<string>('')
   const fieldRef = useRef<HTMLSpanElement>(null)
 
@@ -22,12 +25,32 @@ export default async function Page() {
   const handleCode = async (code: string) => {
     if (!fieldRef.current) return
     fieldRef.current.innerHTML = code
-    const AST = await parseCSS(code)
-    console.log(AST)
+    setFukuCSS(code)
+    // const parsed = JSON.stringify(
+    //   await parseCSS(code)
+    // ) as unknown as ExtractedCSS[]
+    // console.log(parsed)
+    // buildCSS(parsed)
   }
 
-  const mouth = css`
-    width: 100px;
+  // const buildCSS = (css: ExtractedCSS[]) => {
+  //   const result = css
+  //     .map(({ selectors, declarations }) => {
+  //       const selector = selectors.join(',')
+  //       const property = declarations
+  //         .map(({ property, value }) => {
+  //           return `${property}:${value}`
+  //         })
+  //         .join(';')
+  //       return `${selector}{${property}}`
+  //     })
+  //     .join('')
+  //   console.log(result)
+  //   setFukuCSS(result)
+  // }
+
+  const fukuwaraiCSS = css`
+    ${fukuCSS}
   `
 
   return (
@@ -36,17 +59,26 @@ export default async function Page() {
         defaultLanguage="css"
         height="60vh"
         onChange={handleChange}
+        // onMount={(editor: editor.IStandaloneCodeEditor) => {
+        //   editor.addCommand(
+        //     KeyMod.CtrlCmd | KeyCode.KeyS,
+        //     () => handleCode(codeRef.current),
+        //     ''
+        //   )
+        // }}
         width="50%"
       />
       <div>
         <button onClick={() => handleCode(codeRef.current)}>アヌアヌ</button>
         <span ref={fieldRef} />
       </div>
-      <div className="face-root">
-        <div className="face">
-          <div className="right-eye">eye</div>
-          <div className="left-eye">eye</div>
-          <div className={mouth}>mouth</div>
+      <div className={fukuwaraiCSS}>
+        <div className="face-root">
+          <div className="face">
+            <div className="right-eye">eye</div>
+            <div className="left-eye">eye</div>
+            <div className="mouth">mouth</div>
+          </div>
         </div>
       </div>
     </>
