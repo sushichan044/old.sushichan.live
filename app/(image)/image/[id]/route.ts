cb2b8234779c4bc280e5ff7001f96de2559a8337
sushi-chaaaan <mail@sushichan.live>
2023-05-26 13:19:10 +0900

    fix url

--- app/(image)/image/[id]/route.ts
+++ app/(image)/image/[id]/route.ts
@@ -2,5 +2,7 @@ import { redirect } from 'next/navigation'
 
 export async function GET(request: Request) {
   const id = request.url.substring(request.url.lastIndexOf('/') + 1)
-  redirect(`https://imagedelivery.net/i4QA6VLSP0gXyj6-3zNFXg/${id}/public`)
+  const url =
+    'https://imagedelivery.net/i4QA6VLSP0gXyj6-3zNFXg/' + id + '/public'
+  redirect(url)
 }
