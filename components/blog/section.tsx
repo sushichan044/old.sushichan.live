3127ce6812aa5a3d946d6afcec56e4793ec2095e
sushi-chaaaan <mail@sushichan.live>
2023-05-26 17:25:18 +0900

    Add Fukuwarai Loading

--- components/blog/section.tsx
+++ components/blog/section.tsx
@@ -3,10 +3,18 @@ import styles from '@/components/blog/section.module.scss'
 type SectionProps = {
   className?: string
   children: React.ReactNode
+  style?: React.CSSProperties
 }
 
-const Section = ({ children, className }: SectionProps) => (
-  <section className={className ? `${className} ${styles.root}` : styles.root}>
+const Section = ({ children, className, style }: SectionProps) => (
+  <section
+    className={
+      className
+        ? `${className} ${styles['section-root']}`
+        : styles['section-root']
+    }
+    style={style}
+  >
     {children}
   </section>
 )
