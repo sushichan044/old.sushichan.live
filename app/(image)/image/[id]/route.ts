7f06dfd0b9b3fb59fff5f53fd111a1c552f477c0
sushi-chaaaan <mail@sushichan.live>
2023-05-26 13:42:35 +0900

    use URL object

--- app/(image)/image/[id]/route.ts
+++ app/(image)/image/[id]/route.ts
@@ -1,10 +1,10 @@
-// import { redirect } from 'next/navigation'
 import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
   const id = request.url.substring(request.url.lastIndexOf('/') + 1)
-  const url =
-    'https://imagedelivery.net/i4QA6VLSP0gXyj6-3zNFXg/' + id + '/public'
+  const url = new URL(
+    `https://imagedelivery.net/i4QA6VLSP0gXyj6-3zNFXg/${id}/public`
+  )
   return NextResponse.redirect(url)
 }
 
