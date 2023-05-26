cfef55cea9e36edef55f9862eb434fef4553b0b7
sushi-chaaaan <mail@sushichan.live>
2023-05-26 14:06:02 +0900

    nanimo wakaran

--- app/(image)/image/[id]/route.ts
+++ app/(image)/image/[id]/route.ts
@@ -4,7 +4,8 @@ import { NextRequest } from 'next/server'
 export async function GET(request: NextRequest) {
   const nextUrl = request.nextUrl
   nextUrl.searchParams.delete('id')
-  const id = nextUrl.pathname.substring(request.url.lastIndexOf('/') + 1)
+  console.log(nextUrl)
+  const id = nextUrl.pathname.substring(nextUrl.pathname.lastIndexOf('/') + 1)
   redirect(`https://imagedelivery.net/i4QA6VLSP0gXyj6-3zNFXg/${id}/public`)
 }
 
