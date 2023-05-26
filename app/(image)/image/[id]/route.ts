46b694e34ee73370b47acc6040827009e942cfe1
sushi-chaaaan <mail@sushichan.live>
2023-05-26 14:02:08 +0900

    remove id query

--- app/(image)/image/[id]/route.ts
+++ app/(image)/image/[id]/route.ts
@@ -2,13 +2,10 @@ import { redirect } from 'next/navigation'
 import { NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  const id = request.url.substring(request.url.lastIndexOf('/') + 1)
-  console.log(id)
-  const url = new URL(
-    `https://imagedelivery.net/i4QA6VLSP0gXyj6-3zNFXg/${id}/public`
-  )
-  url.searchParams.delete('id')
-  redirect(url.toString())
+  const nextUrl = request.nextUrl
+  nextUrl.searchParams.delete('id')
+  const id = nextUrl.pathname.substring(request.url.lastIndexOf('/') + 1)
+  redirect(`https://imagedelivery.net/i4QA6VLSP0gXyj6-3zNFXg/${id}/public`)
 }
 
 export const runtime = 'edge'
