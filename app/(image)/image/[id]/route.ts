29ebcbd9c4703e1f59144bf982f0b6d9054da92a
sushi-chaaaan <mail@sushichan.live>
2023-05-26 13:46:55 +0900

    add debug console

--- app/(image)/image/[id]/route.ts
+++ app/(image)/image/[id]/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
   const id = request.url.substring(request.url.lastIndexOf('/') + 1)
+  console.log(id)
   const url = new URL(
     `https://imagedelivery.net/i4QA6VLSP0gXyj6-3zNFXg/${id}/public`
   )
