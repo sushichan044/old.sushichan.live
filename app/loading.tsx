af71748eb1298440c1cb013b9b196488a2c8b37e
sushi-chaaaan <mail@sushichan.live>
2023-05-26 17:19:11 +0900

    Add loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+import Section from '@/components/blog/section'
+import MainContainer from '@/components/common/mainContainer'
+
+export default function Page() {
+  return (
+    <MainContainer>
+      <Section>
+        <p>Now Loading...</p>
+      </Section>
+    </MainContainer>
+  )
+}
