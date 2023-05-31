9f833e44124e59b9ff7b91e119e164cb20aef302
sushi-chaaaan <mail@sushichan.live>
2023-05-31 17:19:13 +0900

    🔥 Remove icon from header

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,16 +1,16 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 import Link from 'next/link'
 
 import styles from '@/components/layout/header.module.scss'
 import NavigationMenu from '@/components/navigation/navigationMenu'
-import LogoSvg from '@/public/logo.svg'
+// import LogoSvg from '@/public/logo.svg'
 
 const Header = () => {
   return (
     <header className={styles.container}>
       <div className={styles['header-inside']}>
         <Link className={styles.hero} href="/">
-          <Image alt="site logo" height={50} src={LogoSvg} width={50} />
+          {/* <Image alt="site logo" height={50} src={LogoSvg} width={50} /> */}
           <p className={styles.title}>お寿司だいすきチャンネル</p>
         </Link>
 
