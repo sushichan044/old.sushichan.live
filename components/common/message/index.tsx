66525c2c9cb9a477a14878285950592d8f86dcfb
sushi-chaaaan <mail@sushichan.live>
2023-06-04 02:22:59 +0900

    ♻️ Refactor card color selector in Message

--- components/common/message/index.tsx
+++ components/common/message/index.tsx
@@ -5,21 +5,20 @@ import { RiErrorWarningLine } from 'react-icons/ri'
 import WithBudoux from '@/components/common/budoux'
 import styles from '@/components/common/message/message.module.scss'
 
+const VALID_TYPES = ['warn', 'alert', 'info'] as const
+
 type MessageProps = {
-  type: 'warning' | 'alert' | 'info'
+  type: (typeof VALID_TYPES)[number]
   children: string
 }
 
 const Message = ({ type, children }: MessageProps) => {
+  if (!VALID_TYPES.includes(type)) {
+    type = 'info'
+  }
+
   return (
-    <div
-      className={clsx(
-        styles.card,
-        type === 'warning' && styles.card__warn,
-        type === 'alert' && styles.card__alert,
-        type === 'info' && styles.card__info
-      )}
-    >
+    <div className={clsx(styles.card, styles[`card__${type}`])}>
       <div className={styles.icon}>
         <RiErrorWarningLine color="inherit" size="1.75em" />
       </div>
