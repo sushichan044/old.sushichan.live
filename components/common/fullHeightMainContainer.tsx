e5a12f63c9c54194969a940b7cef5a264b992cb2
sushi-chaaaan <mail@sushichan.live>
2023-06-03 17:50:37 +0900

    ♻️ Use clsx to add class

--- components/common/fullHeightMainContainer.tsx
+++ components/common/fullHeightMainContainer.tsx
@@ -1,9 +1,15 @@
+import clsx from 'clsx'
+
 import styles from '@/components/common/full-height-main-container.module.scss'
 
 export default function FullHeightMainContainer({
   children,
+  className,
 }: {
   children: React.ReactNode
+  className?: string
 }) {
-  return <div className={styles.root}>{children}</div>
+  return (
+    <div className={clsx(styles.root, className && className)}>{children}</div>
+  )
 }
--- components/common/mainContainer.tsx
+++ components/common/mainContainer.tsx
@@ -1,9 +1,15 @@
+import clsx from 'clsx'
+
 import styles from '@/components/common/main-container.module.scss'
 
 export default function MainContainer({
   children,
+  className,
 }: {
   children: React.ReactNode
+  className?: string
 }) {
-  return <div className={styles.root}>{children}</div>
+  return (
+    <div className={clsx(styles.root, className && className)}>{children}</div>
+  )
 }
