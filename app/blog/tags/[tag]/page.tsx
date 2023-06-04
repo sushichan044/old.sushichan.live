060ef03474f9b33c3c0f59c468687f351bdcd876
sushi-chaaaan <mail@sushichan.live>
2023-06-04 16:02:23 +0900

    ✨ Add tag page

--- app/blog/tags/[tag]/page.tsx
+++ app/blog/tags/[tag]/page.tsx
@@ -1,3 +1,7 @@
+import PostGrid from '@/app/blog/_components/postGrid'
+import Section from '@/components/section'
+import { getAllMDXSlugs, getMDXExistence, getMDXFrontMatter } from '@/lib/mdx'
+
 type PageProps = {
   params: {
     tag: string
@@ -7,10 +11,25 @@ type PageProps = {
 export default async function Page({ params: { tag } }: PageProps) {
   const decodedTag = decodeURIComponent(tag)
 
+  const allPosts = (await getAllMDXSlugs({}))
+    .map((slug) => getMDXExistence(slug))
+    .flatMap((mdx) => (mdx.exists ? mdx : []))
+
+  const rawFrontMatters = await Promise.all(
+    allPosts.map((mdx) => getMDXFrontMatter(mdx.fileName, mdx.extension))
+  )
+  const matchedFrontMatters = rawFrontMatters
+    .filter((metaData) => {
+      return metaData.tags?.includes(decodedTag)
+    })
+    .sort((a, b) => {
+      return a.date < b.date ? 1 : -1
+    })
+
   return (
-    <div>
-      <h1>Tag: {decodedTag}</h1>
-    </div>
+    <Section>
+      <h1>{`「${decodedTag}」 のタグがついた記事`}</h1>
+      <PostGrid posts={matchedFrontMatters} />
+    </Section>
   )
 }
-1
