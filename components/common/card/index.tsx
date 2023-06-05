f2d0cc039748220ffb19d548c59c3c9554abcc82
sushi-chaaaan <mail@sushichan.live>
2023-06-05 19:39:58 +0900

    ✅ Card accepts style props

--- components/common/card/index.tsx
+++ components/common/card/index.tsx
@@ -4,11 +4,12 @@ import styles from '@/components/common/card/card.module.scss'
 
 type CardProps = {
   className?: string
+  style?: React.CSSProperties
   rounded?: boolean
   children: React.ReactNode
 }
 
-const Card = ({ children, rounded, className }: CardProps) => {
+const Card = ({ children, rounded, className, style }: CardProps) => {
   return (
     <div
       className={clsx(
@@ -16,6 +17,7 @@ const Card = ({ children, rounded, className }: CardProps) => {
         className && className,
         rounded && styles.round
       )}
+      style={style}
     >
       {children}
     </div>
