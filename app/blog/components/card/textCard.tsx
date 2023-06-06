a6b2fce3dcfa63aff45f205e3d37218d8ad8c0cc
sushi-chaaaan <mail@sushichan.live>
2023-06-06 17:31:25 +0900

    💄 Use budoux in TextCard

--- app/blog/components/card/textCard.tsx
+++ app/blog/components/card/textCard.tsx
@@ -1,8 +1,19 @@
 import styles from '@/app/blog/components/card/text-card.module.scss'
+import WithBudoux from '@/components/common/budoux'
 import Card from '@/components/common/card'
 
-const TextCard = ({ children }: { children: string }) => {
-  return <Card className={styles.card}>{children}</Card>
+const TextCard = ({
+  children,
+  useBudoux = true,
+}: {
+  children: string
+  useBudoux: boolean
+}) => {
+  return (
+    <Card className={styles.card}>
+      {useBudoux ? <WithBudoux>{children}</WithBudoux> : children}
+    </Card>
+  )
 }
 
 export default TextCard
