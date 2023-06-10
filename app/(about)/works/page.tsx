428ccd2df5551a72c42cc523533468e2a2607bfb
sushi-chaaaan <mail@sushichan.live>
2023-06-10 23:13:02 +0900

    📝 edit about page

--- app/(about)/works/page.tsx
+++ app/(about)/works/page.tsx
@@ -1,9 +1,25 @@
+import { notFound } from 'next/navigation'
+
 import Section from '@/components/section'
+import { compileMDX, getMDXFromPath } from '@/lib/mdx'
+
+export const metadata = {
+  title: 'about',
+  description: 'Introduce myself',
+}
 
 export default async function Page() {
-  return (
-    <>
-      <Section>Works</Section>
-    </>
-  )
+  const mdx = getMDXFromPath({ topDirectory: 'md', fileName: 'works' })
+  if (!mdx) {
+    notFound()
+  }
+
+  const content = await compileMDX({
+    isRaw: false,
+    mdxFile: mdx,
+    feature: {
+      generateToc: false,
+    },
+  })
+  return <Section>{content}</Section>
 }
---
+++ md/about.md
@@ -0,0 +1,26 @@
+
+# About me
+
+## Profile
+
+岡山県出身の2004年生まれ・19歳。高校3年時に受験勉強に嫌気が差し、
+プログラミングを始める。それ以降、興味の赴くままに様々な分野を触る。
+最近はWebフロントエンドに興味がある。趣味は音楽を聴くことで、
+エレクトリックな音が好き。[詳しくはこちら](/blog/post/music)
+
+## Skills
+
+- Python
+  - discord.py
+  - FastAPI
+
+- JavaScript(TypeScript)
+  - React
+  - Next.js
+
+- Server
+  - Docker
+
+## Works
+
+[詳しくはこちら](/works)
