094b2d9aea102447b5bc59ee39173188a2ef7984
sushi-chaaaan <mail@sushichan.live>
2023-06-12 22:46:49 +0900

    📝 fix /blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -6,13 +6,10 @@ export default async function Page() {
     <>
       <Section>
         <h1>雑記</h1>
-        <p>
-          ここは徒歩記事から技術メモまでよくわからないコンテンツを書き溜める場です。
-        </p>
+        <p>文字を書きますよ</p>
       </Section>
       <Section>
         <h2>最近書いた記事</h2>
-        {/* FIXME: https://nextjs.org/docs/app/building-your-application/data-fetching/fetching#async-and-await-in-server-components */}
         <RecentPosts />
       </Section>
     </>
