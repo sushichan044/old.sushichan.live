8fec76a030226f4aa44e4f831ebb424627eaa5ac
sushi-chaaaan <mail@sushichan.live>
2023-06-13 16:51:18 +0900

    ♻️ Refactor WithBudoux to use in everywhere

--- components/common/budoux.tsx
+++ components/common/budoux.tsx
@@ -1,23 +1,90 @@
 import { loadDefaultJapaneseParser } from 'budoux'
+import React from 'react'
 
 import styles from '@/components/common/budoux.module.scss'
 
-const WithBudoux = ({ children }: { children?: string }) => {
-  if (!children || children === '') {
-    return <></>
+const WithBudoux = ({ children }: { children?: React.ReactNode }) => {
+  if (
+    !children &&
+    (typeof children !== 'string' ||
+      typeof children !== 'object' ||
+      !Array.isArray(children))
+  ) {
+    return <>{children}</>
+  }
+
+  if (Array.isArray(children)) {
+    return (
+      <>
+        {children.map((child, i) => {
+          if (typeof child === 'string') {
+            return <React.Fragment key={i}>{parseString(child)}</React.Fragment>
+          }
+          if (typeof child === 'object') {
+            return (
+              <React.Fragment key={i}>{parseJSXObject(child)}</React.Fragment>
+            )
+          }
+          return <React.Fragment key={i}>{child}</React.Fragment>
+        })}
+      </>
+    )
+  }
+
+  if (typeof children === 'object') {
+    return <>{parseJSXObject(children)}</>
+  }
+
+  if (typeof children === 'string') {
+    return <>{parseString(children)}</>
+  }
+
+  return <>{children}</>
+}
+
+const parseString = (str: string) => {
+  const parser = loadDefaultJapaneseParser()
+  return parser.parse(str).map((val, i) => (
+    <span className={styles.span} key={i}>
+      {val}
+    </span>
+  ))
+}
+
+const parseJSXObject = (jsx: React.ReactNode) => {
+  if (!jsx || typeof jsx !== 'object') {
+    return jsx
+  }
+
+  const _children = { ...jsx }
+
+  if (
+    !(
+      'type' in _children &&
+      'props' in _children &&
+      'children' in _children.props &&
+      typeof _children.props.children === 'string'
+    )
+  ) {
+    return jsx
   }
 
   const parser = loadDefaultJapaneseParser()
-
-  return (
+  const parsedFragment = (
     <>
-      {parser.parse(children).map((val, i) => (
+      {parser.parse(_children.props.children).map((val, i) => (
         <span className={styles.span} key={i}>
           {val}
         </span>
       ))}
     </>
   )
+
+  const replacedJSX = (
+    <_children.type {..._children.props}>{parsedFragment}</_children.type>
+  )
+
+  return replacedJSX
 }
 
 export default WithBudoux
