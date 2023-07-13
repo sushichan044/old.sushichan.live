387258beade1e790fbe2e103b08585bfad37a82f
sushi-chaaaan <mail@sushichan.live>
2023-07-14 08:09:32 +0900

    📝 show tags in /blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,14 +1,26 @@
+import Link from 'next/link'
+
 import RecentPosts from '@/app/blog/components/recentPosts'
+import Tag from '@/app/blog/components/tag'
+import styles from '@/app/blog/page.module.scss'
 import Section from '@/components/section'
-import { getAllMDXFile, getAllMDXMetaData } from '@/lib/mdx'
+import { getAllMDXFile, getAllMDXMetaData, getAllTagsFromMDX } from '@/lib/mdx'
 export default async function Page() {
   const allPostFiles = getAllMDXFile({ topDirectory: 'posts' })
   const allFrontMatters = getAllMDXMetaData(allPostFiles)
+  const allTags = getAllTagsFromMDX(allFrontMatters)
   return (
     <>
-      <Section>
+      <Section className={styles.description}>
         <h1>雑記</h1>
-        <p>文字を書きますよ</p>
+        <p>文字を書きます。内容はいろいろ</p>
+        <div className={styles.tags}>
+          {allTags?.map((tag) => (
+            <Link href={`/blog/tag/${tag}`} key={tag}>
+              <Tag key={tag} tag={tag} />
+            </Link>
+          ))}
+        </div>
       </Section>
       <Section>
         <h2>最近書いた記事</h2>
