9b9e5ce6761b109dcc5922d39d638b24cd17902c
sushi-chaaaan <mail@sushichan.live>
2023-07-14 05:18:15 +0900

    ⚗️ Add type for MDX Compiler v2

--- lib/mdx/type.ts
+++ lib/mdx/type.ts
@@ -39,3 +39,25 @@ export type MDXCompilerOption =
     ) & {
       feature?: MDXCompilerFeature
     }
+
+// types below will used in v2
+// experimental type
+type MDXFileMetaData = MDXFile
+type MDXFrontMatterBase = {
+  status: 'public' | 'private'
+  created: Date
+  updated: Date
+}
+
+// error if any key in T is in MDXFrontMatterBase
+// will integrate with zod in v2
+type MDXFrontMatter<T extends object> = keyof T extends keyof MDXFrontMatterBase
+  ? never
+  : MDXFrontMatterBase & T
+
+// FUTURE: use this type as MDX general type
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type MDX<T extends object> = {
+  file: MDXFileMetaData
+  metaData: MDXFrontMatter<T>
+}
