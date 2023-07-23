9bc4d5d6a1fe3c8310ced281dfcd9caa3bce3e29
sushi-chaaaan <mail@sushichan.live>
2023-07-24 01:05:55 +0900

    Add props to choose Spotify Card style

--- app/blog/components/card/spotifyCard.tsx
+++ app/blog/components/card/spotifyCard.tsx
@@ -1,12 +1,22 @@
+import clsx from 'clsx'
 import { Spotify } from 'react-spotify-embed'
 
 import styles from '@/app/blog/components/card/spotify-card.module.scss'
 import EmbedCard from '@/components/common/card/embedCard'
 
-const SpotifyCard = ({ url }: { url: string }) => {
+const SpotifyCard = ({
+  url,
+  shape = 'wide',
+}: {
+  url: string
+  shape: 'wide' | 'square'
+}) => {
   return (
     <EmbedCard shadow>
-      <Spotify className={styles.card} link={url} />
+      <Spotify
+        className={clsx(shape === 'wide' ? styles.wide : styles.square)}
+        link={url}
+      />
     </EmbedCard>
   )
 }
