13d0a0a6ce6204df8c7e23aa016c0b8ff12902a6
sushi-chaaaan <mail@sushichan.live>
2023-07-24 16:39:30 +0900

    ✨ add useTimeout hook

---
+++ lib/hooks/useTimeout.ts
@@ -0,0 +1,13 @@
+import { useEffect } from 'react'
+
+export function useTimeout(callback: () => void, delay?: number | undefined) {
+  useEffect(() => {
+    if (!delay && delay !== 0) {
+      return
+    }
+
+    const timeout = setTimeout(() => callback(), delay)
+
+    return () => clearTimeout(timeout)
+  }, [callback, delay])
+}
