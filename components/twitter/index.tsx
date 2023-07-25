dad42f8408b861d2febb4afef1ddf7525ab77ae0
sushi-chaaaan <mail@sushichan.live>
2023-07-26 06:03:16 +0900

    ✏️ fix typo

--- components/twitter/index.tsx
+++ components/twitter/index.tsx
@@ -1,4 +1,4 @@
-import { TwitterTheme } from '@/components/twitter/theme'
 import { TimelineEmbed } from '@/components/twitter/timeline'
 import { TweetEmbed } from '@/components/twitter/tweet'
+import { TwitterTheme } from '@/components/twitter/twitterTheme'
 export { TweetEmbed, TimelineEmbed, TwitterTheme }
