d1977cb0efe4ff435243914e9d1e097eb8f0517f
sushi-chaaaan <mail@sushichan.live>
2023-07-26 06:20:00 +0900

    mark tweetCard as Server Component

--- app/blog/components/card/tweetCard.tsx
+++ app/blog/components/card/tweetCard.tsx
@@ -1,20 +1,20 @@
-'use client'
+// 'use client'
 
 import { Tweet } from 'react-tweet'
 
 import EmbedCard from '@/components/common/card/embedCard'
-import { TwitterTheme } from '@/components/twitter'
-import useClientTheme from '@/lib/hooks/useClientTheme'
+// import { TwitterTheme } from '@/components/twitter'
+// import useClientTheme from '@/lib/hooks/useClientTheme'
 
 const TweetCard = ({ id }: { id: string }) => {
-  const { theme } = useClientTheme()
+  // const { theme } = useClientTheme()
 
   return (
-    <TwitterTheme asChild theme={theme}>
-      <EmbedCard>
-        <Tweet id={id} />
-      </EmbedCard>
-    </TwitterTheme>
+    // <TwitterTheme asChild theme={theme}>
+    <EmbedCard>
+      <Tweet id={id} />
+    </EmbedCard>
+    // </TwitterTheme>
   )
 }
 
