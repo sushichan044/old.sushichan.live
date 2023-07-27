cd6fa8f1e5e7452549f8b2bd9b76d9fdac7d8a89
sushi-chaaaan <mail@sushichan.live>
2023-07-27 18:22:32 +0900

    use parseBoolean()

--- utils/string.ts
+++ utils/string.ts
@@ -1,21 +1,29 @@
-type toggleBoolOptions = {
-  trueValue?: string
-  falseValue?: string
-  fallbackValue?: string
+/**
+ * Parse a boolean-like literal such as `"True"`, `"False"` to a boolean.
+ * If the string is null, it will return false.
+ * If string is not boolean-like, it will return false and log an error.
+ *
+ * @param {(string | null)} str
+ * @return {*}  {boolean}
+ * @example
+ * parseBoolean('true') // true
+ * parseBoolean('True') // true
+ * parseBoolean('TRUE') // true
+ * parseBoolean('false') // false
+ * parseBoolean('False') // false
+ * parseBoolean('FALSE') // false
+ * parseBoolean('ABCDEF') // error and return false
+ * parseBoolean(null) // false
+ */
+const parseBoolean = (str: string | null | undefined): boolean => {
+  if (str === null || str === undefined) return false
+
+  const parsed = str.toLowerCase()
+  if (parsed === 'true') return true
+  if (parsed === 'false') return false
+
+  console.error(`parseBoolean: ${str} is not a valid boolean`)
+  return false
 }
 
-const toggleBoolString = (
-  str: string | null,
-  {
-    trueValue = 'true',
-    falseValue = 'false',
-    fallbackValue = '',
-  }: toggleBoolOptions = {}
-): string => {
-  if (str === trueValue) return falseValue
-  if (str === falseValue) return trueValue
-
-  return fallbackValue
-}
-
-export { toggleBoolString }
+export { parseBoolean }
