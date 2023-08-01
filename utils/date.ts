e57f3d196588a18fd33f7c8e591ae89019c02448
sushi-chaaaan <mail@sushichan.live>
2023-08-02 05:56:16 +0900

    add covertToJST() method

--- utils/date.ts
+++ utils/date.ts
@@ -1,7 +1,20 @@
 import { format } from 'date-fns'
+import { utcToZonedTime } from 'date-fns-tz'
 
 const getFormattedDate = (date: Date, options: { format: string }) => {
   return format(date, options.format, {})
 }
 
-export { getFormattedDate }
+const convertToJST = <T extends Record<string, Date>>(rec: T): T => {
+  const JST = 'Asia/Tokyo'
+  const converted = Object.entries(rec).reduce((acc, [key, value]) => {
+    const convertedDate = utcToZonedTime(value, JST)
+    return {
+      ...acc,
+      [key]: convertedDate,
+    }
+  }, {} as T)
+  return converted
+}
+
+export { convertToJST, getFormattedDate }
