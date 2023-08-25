e116c855fff0d8bcc2cdc50d299b46ed4a93d774
sushi-chaaaan <mail@sushichan.live>
2023-08-26 01:20:12 +0900

    add WithCaption Component

---
+++ components/common/withCaption.tsx
@@ -0,0 +1,28 @@
+import clsx from 'clsx'
+import React from 'react'
+
+import s from '@/components/common/with-caption.module.scss'
+
+type WithCaptionProps = React.ComponentProps<'div'> & {
+  caption?: string
+  isFigure: boolean
+}
+
+const WithCaption = ({
+  children,
+  className,
+  caption,
+  isFigure,
+  ...props
+}: WithCaptionProps) => {
+  const CaptionTag = isFigure ? 'figcaption' : 'p'
+
+  return (
+    <div className={clsx(s.root, className && className)} {...props}>
+      {children}
+      {caption && <CaptionTag>{caption}</CaptionTag>}
+    </div>
+  )
+}
+
+export default WithCaption
