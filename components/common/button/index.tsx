f7d292cdedecd57303060cb466f07136a6159b5c
sushi-chaaaan <mail@sushichan.live>
2023-08-26 00:56:19 +0900

    add role props as required in Button

--- components/common/button/index.tsx
+++ components/common/button/index.tsx
@@ -2,13 +2,14 @@ import styles from '@/components/common/button/button.module.scss'
 
 export type ButtonProps = {
   children: React.ReactNode
+  role: React.ComponentProps<'button'>['role']
   style?: React.CSSProperties
   onClick?: React.ComponentProps<'button'>['onClick'] | undefined
 }
 
-const Button = ({ children, style, onClick }: ButtonProps) => {
+const Button = ({ children, role, style, onClick }: ButtonProps) => {
   return (
-    <button className={styles.root} onClick={onClick} style={style}>
+    <button className={styles.root} onClick={onClick} role={role} style={style}>
       {children}
     </button>
   )
