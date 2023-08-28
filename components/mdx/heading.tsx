51f27270da6395b8a609ad788f2ea810d8940766
sushi-chaaaan <mail@sushichan.live>
2023-08-29 01:59:01 +0900

    💄 update <MDXHeading />

--- components/mdx/heading.tsx
+++ components/mdx/heading.tsx
@@ -1,15 +1,17 @@
+import clsx from 'clsx'
 import React from 'react'
 
 import WithBudoux from '@/components/common/budoux'
+import s from '@/components/mdx/heading.module.scss'
 
 type HeadingProps = {
   level: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'
 } & React.ComponentProps<'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'>
 
-const MDXHeading = ({ level, children, ...props }: HeadingProps) => {
+const MDXHeading = ({ level, children, className, ...props }: HeadingProps) => {
   const Component = level
   return (
-    <Component {...props}>
+    <Component className={clsx(s.heading, className && className)} {...props}>
       {typeof children !== 'string' ? (
         children
       ) : (
