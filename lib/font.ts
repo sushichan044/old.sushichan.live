a5cc2856d72e9c12706ebd7ec3cb2c0b53724b66
sushi-chaaaan <mail@sushichan.live>
2023-08-31 01:18:39 +0900

    perf: remove font variation
    remove suppoerted font weight to reduce css size

--- lib/font.ts
+++ lib/font.ts
@@ -2,14 +2,14 @@ import { Fira_Code, Noto_Sans_JP, Roboto } from 'next/font/google'
 
 const notoSansJP = Noto_Sans_JP({
   display: 'swap',
-  weight: ['100', '400', '700', '900'],
+  weight: ['400', '700'],
   subsets: ['latin'],
   variable: '--font-noto-sans-jp',
 })
 
 const roboto = Roboto({
   display: 'swap',
-  weight: ['100', '400', '700', '900'],
+  weight: ['400', '700'],
   subsets: ['latin'],
   variable: '--font-roboto',
 })
