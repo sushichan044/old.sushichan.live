031510d1eff0aadadedf9905f0e6d21078cb9ba3
sushi-chaaaan <mail@sushichan.live>
2023-09-03 19:57:13 +0900

    use own react-tweet backend

--- components/twitter/tweetBase.tsx
+++ components/twitter/tweetBase.tsx
@@ -3,8 +3,18 @@ import { Tweet } from 'react-tweet'
 import { TweetComponents } from '@/components/twitter/tweet-components'
 import type { TweetProps } from '@/components/twitter/types'
 
+const TWEET_API_URL = process.env.TWEET_API_URL
+
 const TweetBase = ({ id }: TweetProps) => {
-  return <Tweet apiUrl='https://tweet-backend.sushichan.live/:id' components={TweetComponents} id={id} />
+  if (!TWEET_API_URL) return null
+
+  return (
+    <Tweet
+      apiUrl={`${TWEET_API_URL}/tweet/:id`}
+      components={TweetComponents}
+      id={id}
+    />
+  )
 }
 
 export { TweetBase }
