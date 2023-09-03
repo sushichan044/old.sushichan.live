9989ddad4e6f5f9040b2633a4f343db3441758aa
sushi-chaaaan <mail@sushichan.live>
2023-09-03 20:32:12 +0900

    fix: fix <TweetBase />
    use own endpoint and use low-level component

--- components/twitter/tweetBase.tsx
+++ components/twitter/tweetBase.tsx
@@ -1,22 +1,26 @@
-'use client'
-
-import { Tweet } from 'react-tweet'
+import { EmbeddedTweet } from 'react-tweet'
 
 import { TweetComponents } from '@/components/twitter/tweet-components'
 import type { TweetProps } from '@/components/twitter/types'
 
 const TWEET_API_URL = process.env.TWEET_API_URL
 
-const TweetBase = ({ id }: TweetProps) => {
-  if (!TWEET_API_URL) return null
+const TweetBase = async ({ id }: TweetProps) => {
+  if (!TWEET_API_URL) return <></>
 
-  return (
-    <Tweet
-      apiUrl={`${TWEET_API_URL}/tweet/:id`}
-      components={TweetComponents}
-      id={id}
-    />
-  )
+  try {
+    const res = await fetch(`${TWEET_API_URL}/tweet/${id}`)
+    const isJson = res.headers.get('content-type')?.includes('application/json')
+    const data = isJson ? await res.json() : undefined
+    if (res.ok) {
+      return <EmbeddedTweet components={TweetComponents} tweet={data} />
+    }
+  } catch (e) {
+    console.error(e)
+    return <></>
+  }
+
+  return <></>
 }
 
 export { TweetBase }
