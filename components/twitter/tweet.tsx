b985b544c64abc947328fd6d05030f9fd069b2b3
sushi-chaaaan <mail@sushichan.live>
2023-09-04 03:43:13 +0900

    chore: fix Tweet Component
    adjust margin

--- components/twitter/tweet.tsx
+++ components/twitter/tweet.tsx
@@ -1,3 +1,4 @@
+import s from '@/components/twitter/tweet.module.scss'
 import { TweetBase } from '@/components/twitter/tweetBase'
 import { TwitterTheme } from '@/components/twitter/twitterTheme'
 import type { TweetProps } from '@/components/twitter/types'
@@ -6,12 +7,12 @@ const Tweet = ({ id, theme }: TweetProps) => {
   return (
     <>
       {theme === undefined ? (
-        <div>
+        <div className={s.root}>
           <TweetBase id={id} />
         </div>
       ) : (
         <TwitterTheme asChild theme={theme}>
-          <div>
+          <div className={s.root}>
             <TweetBase id={id} />
           </div>
         </TwitterTheme>
