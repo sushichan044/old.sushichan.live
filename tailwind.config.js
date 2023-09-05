01a938f87efd0f72f26bf54fb130794e4585e927
sushi-chaaaan <mail@sushichan.live>
2023-09-06 01:19:57 +0900

    build(config): use tailwind in everywhere as CSS in JS and extend aspect-ratio

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,21 @@ module.exports = {
     preflight: false,
   },
   content: [
+    './app/**/*.{js,ts,jsx,tsx,md,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,md,mdx}',
     './posts/**/*.{js,ts,jsx,tsx,md,mdx}',
     './md/**/*.{js,ts,jsx,tsx,md,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      aspectRatio: {
+        '1200/630': '1200 / 630',
+        '16/9': '16 / 9',
+        '4/3': '4 / 3',
+        '3/2': '3 / 2',
+        '1/1': '1 / 1',
+      },
+    },
   },
   plugins: [],
 }
