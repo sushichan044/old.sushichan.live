b43ed1e1c1d4c00b86b7437a06420170a19ea069
sushi-chaaaan <mail@sushichan.live>
2023-09-08 12:17:42 +0900

    chore: remove cloudflare images feature

--- src/lib/imageLoader.ts
+++ src/lib/imageLoader.ts
@@ -1,17 +1,5 @@
 import { type ImageLoaderProps } from 'next/image'
 
-export function cloudflareLoader({ src, width, quality }: ImageLoaderProps) {
-  const optimizedSrc = src.replace('/public', '')
-
-  const params = [
-    `width=${width}`,
-    `quality=${quality || 75}`,
-    'format=auto',
-    'fit=scale-down',
-  ]
-  return `${optimizedSrc}/${params.join(',')}`
-}
-
 export const getCloudinaryIdentifier = (src: string) => {
   return src.replace('https://res.cloudinary.com/sushi-chan/image/upload/', '')
 }
